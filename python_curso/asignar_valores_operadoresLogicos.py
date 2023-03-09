b2cc540b0b84abf32852c1ed0083f1a4fb664463
Karen Jocelyn <ada_hack109@hackademy.mx>
2023-03-08 18:26:27 -0600

    Asignar Valores con operadores lógicos

---
+++ python_curso/asignar_valores_operadoresLogicos.py
@@ -0,0 +1,8 @@
+"""
+Asignar valores con operadores lógico or es de mucha utilidad
+cuando queremos asignar valores los cuales pertenezcan a otras 
+variables
+"""
+variable= "" or "Karen Jocelyn"
+
+print(variable) #la ssalida va a ser Karen Jocelyn
