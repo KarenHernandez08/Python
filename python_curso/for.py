13817c8fbe701737e604731ee1523f04a4522eb4
Karen Jocelyn <ada_hack109@hackademy.mx>
2023-03-08 18:56:45 -0600

    ciclo for

---
+++ python_curso/for.py
@@ -0,0 +1,20 @@
+"""
+         Ciclo For
+No es mas que un ciclo foreach, es decir que 
+utilizando el ciclo for, seremos capaces de iterar
+sobre cada uno de los elementos de una colección o un objeto 
+iterable, que puede ser, un string, una lista, una tupla, un 
+diccionario,etc
+"""
+usuarios = ["usuario1", "usuario2", "usuario3"]
+
+for usuario in usuarios:
+    print(usuario)
+
+
+""" 
+El ciclo for tiene 4 elementos que lo componen
+    1. For
+    2. Una variable que tomara el valor de cada uno de los elementos
+    3. La iteracion
+    4. ElObjeto que queremos iterar"""
