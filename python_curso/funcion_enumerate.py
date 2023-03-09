956f6ecb1b407df4e56609ca81b9952f92d1eade
Karen Jocelyn <ada_hack109@hackademy.mx>
2023-03-08 19:38:28 -0600

    range

---
+++ python_curso/funcion_enumerate.py
@@ -0,0 +1,20 @@
+"""
+          Función range
+seremos capaces de crear una secuencia de numeros
+enteros que facilmente se pueden iterar
+
+range(10) #va del 0 al 10
+
+range(1,10) #va del 1 al 10, el primer numeró es donde va a 
+iniciar nuestra secuencia y el segundo donde acaba
+
+range(1,10,2) va deñ 1 al 0 pero con saltos de 2 en dós
+el tercer valor son los saltos
+"""
+
+
+
+rango= range(1,10,2) #va del 0 al 10
+
+for valor in rango:
+    print(valor)
