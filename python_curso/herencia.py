d29e9738435d9de42fd9aa135c657ef5ac95d969
Karen Jocelyn <ada_hack109@hackademy.mx>
2023-05-30 12:49:28 -0600

    herencia múltiple

--- python_curso/herencia.py
+++ python_curso/herencia.py
@@ -16,3 +16,35 @@ class Perro(Mascota): #clase hija, entre parentesis se pone la clase que vamos a
 duke = Perro()#la clase hija 
 duke.comer()#imprime el mensaje que esta en la clase mascota(padre)
 duke.dormir()
+
+
+
+"""
+      HERENCIA MULTIPLE
+python permite la herencia multiple, una clase puede heredar de 
+multiples clases
+"""
+class Animal:
+    def comer (self):
+        print('El animal come')
+
+    def dormir(self):
+        print('El animal duerme')
+
+
+class Mascota(Animal): #clase padre ouede tabien heredar
+
+    pass
+
+class Felino:
+    def cazar(self):
+        print('el perro caza')
+
+class Perro(Mascota, Felino): #las clases separadas por una , aquellas clases a heredar
+
+    pass
+
+duke = Perro()#la clase hija 
+duke.comer()#imprime el mensaje que esta en la clase mascota(padre)
+duke.dormir()
+duke.cazar()#este de de la clase felino
