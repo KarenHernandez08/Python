151837e0272cdfef8015a94d039d467f421b06f5
Karen Jocelyn <ada_hack109@hackademy.mx>
2023-05-30 00:12:43 -0600

    documentar funciones

---
+++ python_curso/documentar_funciones.py
@@ -0,0 +1,21 @@
+#DOCSTRING : Un comentario en la primera línea de código de la función
+#colocamos una breve descripción de la tarea de la función
+
+#__doc__ (modulos, clases, métodos, funciones) 
+
+def suma(n1, n2):
+    """ 
+    La función suma dos numeros enteros.
+
+    Argumentos:
+    n1(int)
+    n2(int)
+
+    se retorna la suma de los parametros.
+
+    TODO:
+        +
+    """
+    return n1 + n2
+#print(suma.__doc__) #esto da la documentación 
+print(help(suma))# esta también nos ayudara a dar la documentación
